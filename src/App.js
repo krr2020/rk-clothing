0e00167753d6f1f02e50a0517e6d77b6efcd097a
KRR <krr8967@gmail.com>
2022-02-14 09:39:31 +0530

    Collection preview, item component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 import HomePage from './pages/homepage/homepage';
+import ShopPage from './pages/shop/shop.jsx';
 
 function App() {
   return (
-    <div><HomePage /></div>
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
+    </div>
   );
 }
 
